29366e7894d2c1b5ebe976d4efc92e8281276bad
thenaughtychid <f2014768@pilani.bits-pilani.ac.in>
2017-07-07 14:49:17 +0530

    completed almost

--- js/script.js
+++ js/script.js
@@ -1,46 +1,60 @@
-$(function(){
+$(function() {
     $('#SubcribeBtn').click(function() {
-        var subscribeForm = {action : document.orderproductForm.action.value,
-           email : $('#email_contactForm').val()           
-        }
-        var email = $('#email_subcribe').val();
-		var request = $.ajax({
-		  url: "https://nodejs-rest-api.herokuapp.com/api/subscribe",
-		  type: "POST",
-		  data: {form : subscribeForm},
-		  dataType: "json"
-		});
+        var data = { 
+        	"form":{
+        		"email": $('#email_subcribe').val() 
+        	}
+        };
+        alert(JSON.stringify(data));
+        var request = $.ajax({
+            url: "https://nodejs-rest-api.herokuapp.com/api/subscribe",
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data),
+            // data: { json_string:JSON.stringify({form : json_string:JSON.stringify(subscribeForm)}) },
+            dataType: "json"
+        });
 
-		request.done(function(msg) {
-		  // $("#log").html( msg );
-		  alert("Success: " + msg);
-		});
+        request.done(function(msg) {
+            // $("#log").html( msg );
+            alert("Success: " + msg);
+        });
 
-		request.fail(function(jqXHR, textStatus) {
-		  alert( "Request failed: " + textStatus );
-		});
+        request.fail(function(jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
+        });
     });
     $('#contactFormBtn').click(function() {
-        var contactForm = {action : document.orderproductForm.action.value,
-           name : $('#name').val(),
-           email : $('#email_contactForm').val(),
-           subject : $('#subject').val(),
-           message : $('#message').val()
+        var data = {         
+        "form": {    
+                "name": $('#name').val(),
+                "email": $('#email_contactForm').val(),
+                "subject": $('#subject').val(),
+                "message": $('#message').val()     
+                }       
+        };
+        var contactForm = {
+            "name": $('#name').val(),
+            "email": $('#email_contactForm').val(),
+            "subject": $('#subject').val(),
+            "message": $('#message').val()
         }
-		var request = $.ajax({
-		  url: "https://nodejs-rest-api.herokuapp.com/contactForm",
-		  type: "POST",
-		  data: {form : contactForm},
-		  dataType: "json"
-		});
+        var request = $.ajax({
+            url: "https://nodejs-rest-api.herokuapp.com/api/contactForm",
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data),
+            // data: { json_string:JSON.stringify({form : json_string:JSON.stringify(contactForm)}) },
+            dataType: "json"
+        });
 
-		request.done(function(msg) {
-		  // $("#log").html( msg );
-		  alert("Success: " + msg);
-		});
+        request.done(function(msg) {
+            // $("#log").html( msg );
+            alert("Success: " + msg);
+        });
 
-		request.fail(function(jqXHR, textStatus) {
-		  alert( "Request failed: " + textStatus );
-		});
+        request.fail(function(jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
+        });
     });
 });
